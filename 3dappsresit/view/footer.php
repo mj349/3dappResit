728a75353f0b166b90d17f3f7762b51f2c527896
mj349 <mj349@sussex.ac.uk>
2022-08-15 07:28:06 +0100

    everything should be here now
    everything has been copied over

---
+++ 3dappsresit/view/footer.php
@@ -0,0 +1,27 @@
+﻿ <div class="container-fluid padding">
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <hr class="light">
+                    <h5>Customer Service</h5>
+                    <p> Contact us/FAQs </p>
+                    <p> Delivery Information </p>
+                    <p> Refunds + Returns </p>
+                    <p> Cookie Settings </p>
+                </div>
+                <div class="col-md-4">
+                    <hr class="light">
+                    <h5>About Your Coca Cola</h5>
+                    <p>Recycling Information</p>
+                    <p>About Us</p>
+                    <p>Refer a Friend</p>
+					<a href="https://github.com/mj349/3dapps.git" class="btn btn-primary btn-responsive">Link to github archive</a>
+                </div>
+                <div class="col-md-4">
+                    <hr class="light">
+                    <h5>Terms and Conditions</h5>
+                    <p>Privacy Notice</p>
+                    <p>Cookie Notice</p>
+                    <p>Terms and Conditions</p>
+                </div>
+            </div>
+ </div>
